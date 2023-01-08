1a2cef0b51311936c1231f1ace05fdea65a71b7e
DevSMike <likashev.migha@yandex.ru>
2023-01-08 17:54:12 +0300

    feat: добавить тесты для + - *,проверка разделения на пробел и вычисление при неизвестной операции

--- test/notation/ReversePolishNotationCalculatorTest.java
+++ test/notation/ReversePolishNotationCalculatorTest.java
@@ -1,11 +1,44 @@
 package notation;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import java.util.NoSuchElementException;
 
 public class ReversePolishNotationCalculatorTest {
 
+    private final ReversePolishNotationCalculator calculator = new ReversePolishNotationCalculator();
     @Test
     public void shouldCalculateAddition() {
+        Assertions.assertEquals(7, calculator.calculatePolishNotation("4 3 +"));
+    }
 
+    @Test
+    public void shouldCalculateSubtraction() {
+        Assertions.assertEquals(0, calculator.calculatePolishNotation("1 1 -"));
+    }
+
+    @Test
+    public void shouldCalculateMultiplication() {
+        Assertions.assertEquals(6, calculator.calculatePolishNotation("2 3 *"));
+    }
+
+    @Test
+    public void shouldCalculateNegativeNumbers() {
+        Assertions.assertEquals(-2, calculator.calculatePolishNotation("-1 -1 +"));
+    }
+    @Test
+    public void shouldNotCalculateNumbersNotSplitBySpace() {
+        Assertions.assertThrows(NoSuchElementException.class, getExecutableForCalculator("1011 *"));
+    }
+
+    @Test
+    public void shouldNotCalculateUnknownOperation() {
+        Assertions.assertThrows(NumberFormatException.class, getExecutableForCalculator("-1 10 /"));
+    }
+
+    private Executable getExecutableForCalculator(String str) {
+        return () -> calculator.calculatePolishNotation(str);
     }
 }
